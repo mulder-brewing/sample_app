6d7a619d6a699c5c3bedecf0984a7bcf1ce6a4f4
Code Monkey <code.monkey@codemonkey.com>
2019-05-08 16:01:39 -0400

    Finished remember me option when logging in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,8 +6,14 @@ module SessionsHelper
 
   #Returns the current logged-in user (if any).
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies.signed[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
@@ -18,7 +24,20 @@ module SessionsHelper
 
   #Logs out the current user.
   def log_out
-    session.delete(:user_id)
-    @current_user = nil
+      forget(current_user)
+      session.delete(:user_id)
+      @current_user = nil
+  end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent.signed[:remember_token] = user.remember_token
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 end
