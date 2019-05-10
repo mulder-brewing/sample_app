0b02e050247875e56afafc36a5bf0cdd607d7d60
Code Monkey <code.monkey@codemonkey.com>
2019-05-10 13:26:02 -0400

    another email tweak

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,8 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   host = 'https://secure-chamber-36248.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-reply@example.com'}
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
