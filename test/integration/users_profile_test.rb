63e2621a4c6dd19cd896bfd87f4ce356007f9b7c
Code Monkey <code.monkey@codemonkey.com>
2019-05-14 09:32:44 -0400

    Add user following

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -18,5 +18,8 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
+    log_in_as @user
+    get root_path
+    assert_select 'div.stats'
   end
 end
